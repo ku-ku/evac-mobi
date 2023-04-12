aa2cacff3f159bbe9190d47d15541761f0a773ad
ReuderGym <dimasiklas03@mail.ru>
2023-04-12 09:12:59 +0300

    Some changes

--- components/EvaGovList.vue
+++ components/EvaGovList.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="eva-govs">
     <v-list v-if="(govs.length > 0)">
-      <v-list-item v-for="gov in govs2" 
+      <v-list-item v-for="gov in govs" 
                    :key="gov.id" 
-                   v-on:click='EvaGovDo'>
+                   v-on:click='govs'>
         <v-list-item-title>
           {{ gov.govnum }}
             <div class="dt">
@@ -18,7 +18,7 @@
   </div>
 </template>
 <script>
-const _LS_KEY = 'saved-govs';
+//const _LS_KEY = 'saved-govs';
 const moment = require("moment");
 moment.locale('ru');
 
@@ -30,15 +30,39 @@ moment.locale('ru');
       }
     },
 
-    async created(){
+    /*async created(){
       //this.govs = JSON.parse(localStorage.getItem(_LS_KEY)) || [];
       //this.$store.dispatch('EvaGovDo')
-    },
+    },*/
     methods: {
+      go() {
+        this.$store.commit("settings/go")
+      },
+      save(){
+        this.$store.commit("settings/save")
+      }
+
+      /*go(id) {
+        const n = this.govs.findIndex( gov => gov.id === id );
+        this.govs[n].dt = new Date();
+        this.govs[n] = this.$store.getters["settings/govs"];
+        this.$emit('go',this.govs[n]);
+      },
+      save(gov){
+        const n = this.govs.findIndex( _gov => _gov.id === gov.id );
+        if ( n < 0){
+          gov.dt = new Date();
+          //this.govs.push(gov);
+        }
+        else {
+          this.govs[n].dt = new Date();
+        }
+        this.govs[n] = this.$store.getters["settings/govs"];
+      }*/
     },
     computed: {
-      gov(){
-        return this.$store.getters(settings/gov);
+      govs(){
+        return this.$store.getters["settings/govs"];
       }
       /*govs2(){
         return this.govs?.map( g => {
@@ -48,11 +72,12 @@ moment.locale('ru');
           console.log(g1, g2);
           return g2.dt.getTime() - g1.dt.getTime();
         }) || []*/
-      }
-    };
+    }
+  }
 
-  
+
 </script>
+
 <style lang="scss" scoped>
   .eva-govs{
     & .v-list{
--- store/settings.js
+++ store/settings.js
@@ -1,14 +1,9 @@
 const _LS_SETTS_KEY = '_eva_setts';
-const _LS_KEY = 'saved-govs';
+//const _LS_KEY = 'saved-govs';
 
 var PushController;
 
 export const state = () => ({
-    govs: {
-        govnums: 1,
-        dt: new Date(),
-        id: 1000
-    },
     notifi: {
         fcmId: undefined
     },
@@ -31,39 +26,37 @@ export const mutations = {
         try {
             const s = window.localStorage.getItem(_LS_SETTS_KEY);
             if ( (s) && /^\{+/.test(s) ){
-                    return this.govs?.map( g => {
-                      g.dt = moment(g.dt).toDate();
+                state.saved = JSON.parse(s);
+                    state.saved.govs = state.saved.govs?.map( g => {
+                      g.dt = moment(g.dt);
                       return g;
                     }).sort( (g1, g2) => {
-                      console.log(g1, g2);
-                      return g2.dt.getTime() - g1.dt.getTime();
-                    }) || [],
-                state.saved = JSON.parse(s);
+                      return g2.dt.isBefore(g1.dt) ? -1 : 1;
+                    }) || [];
             }
         } catch(e){
             console.log('ERR (ls-read saved)', e);
         }
     },
-    do(){
-        this.govs = JSON.parse(localStorage.getItem(_LS_KEY)) || [];
-    },
-    go(id) {
-        const n = this.govs.findIndex( gov => gov.id === id );
-        gov.dt = new Date();
-        localStorage.setItem(_LS_KEY, JSON.stringify(this.govs));
-        this.$emit('go',this.gov);
-    },
-    save(gov){
-        const n = this.govs.findIndex( _gov => _gov.id === gov.id );
+    go(state, id) {       
+        const n = state.govs.findIndex( gov => gov.id === id );
+        this.govs[n].dt = new Date();
+        this.govs[n] = state.saved.govs; //this.$store.getters["settings/govs"];
+        this.$emit('go',this.govs[n]);
+      },
+    save(state, gov){
+        const n = state.govs.findIndex( _gov => _gov.id === gov.id );
         if ( n < 0){
-          gov.dt = new Date();
+          this.gov.dt = new Date();
           this.govs.push(gov);
+          this.$emit('save', this.gov.dt)
         }
         else {
           this.govs[n].dt = new Date();
+          this.$emit('save', this.govs[n].dt);
         }
-        localStorage.setItem(_LS_KEY, JSON.stringify(this.govs));
-      },
+        this.govs[n] = state.saved.govs; //this.$store.getters["settings/govs"];
+    },
     setSaved(state, payload) {
         const stt = state.saved || {};
         Object.keys(payload).forEach( k => {
@@ -112,10 +105,6 @@ export const actions = {
                 });
             }
         };
-        /*EvaGovDo({commit}); {
-            commit('go', id)
-            commit('save', gov)
-        }*/
         return new Promise(_p);
     },   //initPushes
     
@@ -134,8 +123,8 @@ export const actions = {
 };
 
 export const getters = {
-    go: state => g =>{
-        return this.$store.setItem({_LS_KEY: this.govs});
+    govs: state => {
+        return state.saved.govs;
     },
     env: state => q =>{
         return (!!q) ? state.env[q] : state.env;
